99a02bdd4b5d5caba89335bcceecc0b01a2855b3
henriquezjassis <henriquezjassis@gmail.com>
2020-10-01 06:04:14 -0300

    rot13 exercise

---
+++ rankup/rot13.py
@@ -0,0 +1,8 @@
+
+
+def rot13(message):
+	myTable = message.maketrans("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz","NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm")
+	return message.translate(myTable)
+   
+
+print(rot13("Test"))
